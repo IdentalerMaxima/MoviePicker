e6a5989d3786874dea4c8d8a9a7ab12851a7c482
IdentalerMaxima <Koppany.naray@gmail.com>
2022-11-16 17:38:27 +0100

    Refactored, cleaned up.

--- src/main/java/Service/xmlRead.java
+++ src/main/java/Service/xmlRead.java
@@ -16,39 +16,32 @@ import java.io.IOException;
 import java.util.Random;
 
 public class xmlRead {
-    private static final String FILE = "C:\\Users\\koppa\\Desktop\\MoviePicker\\" +
+
+    static final String FILE = "C:\\Users\\koppa\\Desktop\\MoviePicker\\" +
             "src\\main\\java\\Resources\\movies.xml";
 
     DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 
-    public JSONArray Read(){
+    //Reads xml to JSONArray
+    public JSONArray Read() {
 
         JSONArray movies = new JSONArray();
 
-        try{
+        try {
             DocumentBuilder builder = dbf.newDocumentBuilder();
             File f = new File(FILE);
             Document xml = builder.parse(f);
             xml.normalize();
+
+            //Saving movies by node
             NodeList nodeList = xml.getElementsByTagName("movie");
 
-            for(int i = 0; i < nodeList.getLength(); i++){
+            for (int i = 0; i < nodeList.getLength(); i++) {
                 Node node = nodeList.item(i);
                 Element movie = (Element) node;
                 JSONObject movieObj = new JSONObject();
 
-                String title = movie.getElementsByTagName("title").item(0).getTextContent();
-                Integer rating = Integer.valueOf(movie.getElementsByTagName("rating").item(0).getTextContent());
-                Integer year = Integer.valueOf(movie.getElementsByTagName("year").item(0).getTextContent());
-                String director = movie.getElementsByTagName("director").item(0).getTextContent();
-                String genre = movie.getElementsByTagName("genre").item(0).getTextContent();
-
-                movieObj.put("title", title);
-                movieObj.put("rating", rating);
-                movieObj.put("year", year);
-                movieObj.put("director", director);
-                movieObj.put("genre", genre);
-
+                readXml(movieObj, movie);
                 movies.put(movieObj);
             }
 
@@ -58,6 +51,7 @@ public class xmlRead {
         return movies;
     }
 
+    //Returns random JSONObject from db
     public JSONObject getRandomMovie() throws ParserConfigurationException {
 
         JSONObject movieObj = new JSONObject();
@@ -74,21 +68,26 @@ public class xmlRead {
             Node node = nodeList.item(random.nextInt(nodeList.getLength()));
             Element movie = (Element) node;
 
-            String title = movie.getElementsByTagName("title").item(0).getTextContent();
-            Integer rating = Integer.valueOf(movie.getElementsByTagName("rating").item(0).getTextContent());
-            Integer year = Integer.valueOf(movie.getElementsByTagName("year").item(0).getTextContent());
-            String director = movie.getElementsByTagName("director").item(0).getTextContent();
-            String genre = movie.getElementsByTagName("genre").item(0).getTextContent();
-
-            movieObj.put("title", title);
-            movieObj.put("rating", rating);
-            movieObj.put("year", year);
-            movieObj.put("director", director);
-            movieObj.put("genre", genre);
+            readXml(movieObj, movie);
 
         } catch (IOException | SAXException e) {
             throw new RuntimeException(e);
         }
         return movieObj;
+    }
+
+    //Loops trough xml and gives back JSONObject
+    private void readXml(JSONObject movieObj, Element movie) {
+        String title = movie.getElementsByTagName("title").item(0).getTextContent();
+        Integer rating = Integer.valueOf(movie.getElementsByTagName("rating").item(0).getTextContent());
+        Integer year = Integer.valueOf(movie.getElementsByTagName("year").item(0).getTextContent());
+        String director = movie.getElementsByTagName("director").item(0).getTextContent();
+        String genre = movie.getElementsByTagName("genre").item(0).getTextContent();
+
+        movieObj.put("title", title);
+        movieObj.put("rating", rating);
+        movieObj.put("year", year);
+        movieObj.put("director", director);
+        movieObj.put("genre", genre);
     }
 }
--- src/main/java/Service/xmlWrite.java
+++ src/main/java/Service/xmlWrite.java
@@ -6,31 +6,33 @@ import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.JAXBException;
 import jakarta.xml.bind.Marshaller;
 import jakarta.xml.bind.Unmarshaller;
+
 import java.io.File;
 import java.util.ArrayList;
 
-
+//Uses JAXB
 public abstract class xmlWrite {
 
-    public static Movies unMarshaling() throws JAXBException
-    {
+    static final String xmlPath = "C:\\Users\\koppa\\Desktop\\" +
+            "MoviePicker\\src\\main\\java\\Resources\\movies.xml";
+
+    //Reads xml to list and adds movie
+    public static Movies unMarshaling() throws JAXBException {
         JAXBContext jaxbContext = JAXBContext.newInstance(Movies.class);
         Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
 
-        Movies movies = (Movies) jaxbUnmarshaller.unmarshal( new File("C:\\Users\\koppa\\Desktop\\MoviePicker\\" +
-                "src\\main\\java\\Resources\\moviesTest.xml") );
+        Movies movies = (Movies) jaxbUnmarshaller.unmarshal(new File(xmlPath));
 
         Movies movieList = new Movies();
         movieList.setMovies(new ArrayList<>());
 
-        for(Movie movie : movies.getMovies())
-        {
+        for (Movie movie : movies.getMovies()) {
             movieList.getMovies().add(movie);
         }
         return movieList;
     }
-    public static void marshaling(Movie movieToAdd) throws JAXBException
-    {
+
+    public static void marshaling(Movie movieToAdd) throws JAXBException {
         JAXBContext jaxbContext = JAXBContext.newInstance(Movies.class);
         Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
 
@@ -43,8 +45,7 @@ public abstract class xmlWrite {
         jaxbMarshaller.marshal(movies, System.out);
 
         //Marshal the movieList in file
-        jaxbMarshaller.marshal(movies, new File("C:\\Users\\koppa\\Desktop\\MoviePicker\\src\\" +
-                "main\\java\\Resources\\movies.xml"));
+        jaxbMarshaller.marshal(movies, new File(xmlPath));
     }
 }
 
